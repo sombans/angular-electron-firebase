d4626e8624baa5bd6896c37ca06feb6a40c56da0
sombans <sombans@ptt.rs>
2018-06-02 00:31:09 +0200

    angular + electron

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width:1200,
+        height:800,
+        icon:__dirname+'/img/dent.png',
+        backgroundColor: '#420024'
+    });
+
+    win.loadURL(`file:${__dirname}/dist/dent/index.html`)
+
+    win.webContents.openDevTools(); 
+
+    win.on('clossed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
